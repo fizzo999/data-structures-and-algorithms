b09b9864c05bad9a22b99ddec9f2f73691038324
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-05-09 18:43:51 -0700

    fixed tests for fifoAnimalShelter

--- javascript/code-challenges/fifoAnimalShelter/__test__/fifo-animal-shelter.test.js
+++ javascript/code-challenges/fifoAnimalShelter/__test__/fifo-animal-shelter.test.js
@@ -1,33 +1,35 @@
 'use strict'
 
 const AnimalShelter = require('../fifo-animal-shelter.js');
-// const Animal = require('../animal')
+const Animal = require('../animal.js');
 
-xdescribe('ANIMAL SHELTER',()=>{
-    const shelter = new AnimalShelter()
-    const cat1  = new Animal('whisk','cat')
+describe('ANIMAL SHELTER',()=>{
+    const shelter = new AnimalShelter();
+    let cat1 = new Animal('testCat001','cat')
+
     it('can add a dog to the shelter',()=>{
-        let dog = new Animal('ruff','dog')
+        let dog = new Animal('Scoobeedoo','dog')
         shelter.enqueue(dog)
         expect(shelter.dog.length).toEqual(1)
         expect(shelter.dog[0]).toEqual(dog)
     })
 
     it('can add multiple cats to the shelter',()=>{
-        let cat2 = new Animal('mew','cat')
+        let cat2 = new Animal('testCat002','cat')
         shelter.enqueue(cat1)
         shelter.enqueue(cat2)
         expect(shelter.cat.length).toEqual(2)
-        expect(shelter.cat[shelter.cat.next]).toEqual(cat1)
+        // expect(shelter.cat[shelter.cat.next]).toEqual(cat1)
+        expect(shelter.cat[0]).toEqual(cat1)
+        expect(shelter.cat[1]).toEqual(cat2)
     })
 
     it('gives an error when entering an animal that is not a cat/dog',()=>{
-        let pig = new Animal('oink','pig')
-        expect(shelter.enqueue(pig)).toEqual("We only take cats and dogs")
+        let ferret = new Animal('bozo','ferret')
+        expect(shelter.enqueue(ferret)).toEqual('We only take cats and dogs')
     })
 
     it('not adding a preference on dequeue should return null',()=>{
-        console.log('dequee',shelter.cat)
         expect(shelter.dequeue()).toEqual(null)
     })
 
